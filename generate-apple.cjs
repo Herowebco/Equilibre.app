10d97b1a5e95adb9f8f65f554158bc46cf4144db
Mustapha Fattah <must.fattah@gmail.com>
2026-04-14 11:49:58 +0100

    Updated generate-apple.cjs

--- generate-apple.cjs
+++ generate-apple.cjs
@@ -1,21 +1,21 @@
 const jwt = require('jsonwebtoken');
 
-// Ta clé privée (Le contenu du Fichier .p8)
-const privateKey = `-----BEGIN PRIVATE KEY-----
-MIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgT4v4jwd42v7TK3ll
-BLp2oZYaNcz5/BNgDRtqXhvEcxWgCgYIKoZIzj0DAQehRANCAAQNCOVtOPMg1/Xc
-fRs9RzII4Uy5xZnffZT+yPblT7yAfS4cFAummnDEcUwYRvP0UbT3TiXWyp1n1PZ9
-TEAVVj9T
------END PRIVATE KEY-----`;
+// Clé formatée de manière stricte sans aucun espace parasite
+const privateKey = 
+"-----BEGIN PRIVATE KEY-----\n" +
+"MIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgT4v4jwd42v7TK3ll\n" +
+"BLp2oZYaNcz5/BNgDRtqXhvEcxWgCgYIKoZIzj0DAQehRANCAAQNCOVtOPMg1/Xc\n" +
+"fRs9RzII4Uy5xZnffZT+yPblT7yAfS4cFAummnDEcUwYRvP0UbT3TiXWyp1n1PZ9\n" +
+"TEAVVj9T\n" +
+"-----END PRIVATE KEY-----";
 
-// Création du JWT
 const token = jwt.sign({}, privateKey, {
   algorithm: 'ES256',
-  expiresIn: '180d', // Valable environ 6 mois
+  expiresIn: '180d',
   audience: 'https://appleid.apple.com',
-  issuer: 'A9W9AX76V8', // Ton Team ID
-  subject: 'com.app.equilibre', // Ton Bundle ID
-  keyid: '243YL9FZUD', // Ton Key ID
+  issuer: 'A9W9AX76V8', 
+  subject: 'com.app.equilibre', 
+  keyid: '243YL9FZUD', 
 });
 
 console.log("--------------------------------------------------");
